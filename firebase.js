2fe119e53b44b6402c005f1493784a6a69622c4e
sctovrp <sctovarp@unal.edu.co>
2020-10-03 11:57:40 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,1 @@
-var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
-
-module.exports.fire = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://westudy-3c433.firebaseio.com"
-});
