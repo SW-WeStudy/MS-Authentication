fda6206c21b5524b1cf214cb2f5cac02b153d09a
sctovrp <sctovarp@unal.edu.co>
2020-12-12 15:14:45 -0500

    added something...

---
+++ firebase.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+module.exports.fire = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://westudy-3c433.firebaseio.com"
+});
